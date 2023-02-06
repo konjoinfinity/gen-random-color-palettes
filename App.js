eb2edd87a2fa014edb095cb84a8d8393c987d7fa
Wesley Scholl <clients@konjoweb.com>
2023-02-06 13:17:40 -0500

    fixed boolean leak

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 
 
 import { useState, useEffect } from 'react';
-import { Button, Text, TouchableOpacity, View, Dimensions } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import * as Clipboard from 'expo-clipboard';
 import ColorConvert from 'color-convert'
 
@@ -25,7 +25,7 @@ function getPalette() {
       })
         .then(res => res.json())
         .then(res => {
-          var palette = [{id: 1, color: `rgb(${res.result[0][0]},${res.result[0][1]},${res.result[0][2]})`, hex: ColorConvert.rgb.hex(res.result[0])},{id: 2, color: `rgb(${res.result[1][0]},${res.result[1][1]},${res.result[1][2]})`,hex: ColorConvert.rgb.hex(res.result[1])},
+          let palette = [{id: 1, color: `rgb(${res.result[0][0]},${res.result[0][1]},${res.result[0][2]})`, hex: ColorConvert.rgb.hex(res.result[0])},{id: 2, color: `rgb(${res.result[1][0]},${res.result[1][1]},${res.result[1][2]})`,hex: ColorConvert.rgb.hex(res.result[1])},
           {id: 3, color: `rgb(${res.result[2][0]},${res.result[2][1]},${res.result[2][2]})`,hex: ColorConvert.rgb.hex(res.result[2])},{id: 4, color: `rgb(${res.result[3][0]},${res.result[3][1]},${res.result[3][2]})`, hex: ColorConvert.rgb.hex(res.result[3])},
           {id: 5, color: `rgb(${res.result[4][0]},${res.result[4][1]},${res.result[4][2]})`,hex: ColorConvert.rgb.hex(res.result[4])}]
           setColors(palette)
@@ -44,7 +44,7 @@ function getPalette() {
       <View style={{display: "flex", flexDirection: "row", flexWrap: "wrap", alignItems:"center", justifyContent:"center", marginBottom: 20}}>
     {colors.map(color => {
       return <View key={color.id}><TouchableOpacity style={{height: 140, width: 140, borderRadius: 5, backgroundColor: color.color, margin: 10}} onPress={() => copyToClipboard(color.hex)} /> 
-      {color.hex && <Text selectable={true} style={{textAlign: 'center', fontSize: 25, fontWeight: '300'}}>#{color.hex}</Text>}</View>})}
+      {color.hex ? <Text selectable={true} style={{textAlign: 'center', fontSize: 25, fontWeight: '300'}}>#{color.hex}</Text> : null}</View>})}
      </View>
      <TouchableOpacity style={{backgroundColor: '#007AFF', padding: 20, borderRadius: 5}} onPress={() => getPalette()}><Text style={{textAlign: 'center', fontSize: 25, color: "white", fontWeight: '400'}}>Generate</Text></TouchableOpacity>
     </View>
